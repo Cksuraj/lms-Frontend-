014f3da0bf8d84d2daef9a7f780d314667c5e407
Cksuraj <sjchaudhary00@gmail.com>
2023-12-28 22:44:17 +0530

    Not Found page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './Pages/Home'
 import HomeLayout from './Layout/Homelayout'
 import Aboutus from './Pages/Aboutus'
+import Notfound from './Pages/Notfound'
 
 function App() {
   // Routes Are container or Tree Which Provides the Branch Route to visit the other path. 
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={ <Home />} />
     <Route path='/' element={<HomeLayout />}> </Route>
     <Route path='/about' element={<Aboutus />}></Route> 
+    <Route path='*' element={<Notfound />}></Route> 
   </Routes>
   </>
  )
